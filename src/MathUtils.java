be31f995e7e2a5a303fb1d27821e695c149b97c7
Melly Dwiliani <mellydwiliani7@gmail.com>
2024-10-25 15:21:22 +0700

    add

---
+++ src/MathUtils.java
@@ -0,0 +1,23 @@
+// ****************************************************************
+// MathUtils.java
+//
+// Provides static mathematical utility functions.
+//
+// ****************************************************************
+public class MathUtils{
+    //-------------------------------------------------------------
+    // Returns the factorial of the argument given
+    //-------------------------------------------------------------
+    public static int factorial(int n) throws IllegalArgumentException{
+        if (n < 0) {
+            throw new IllegalArgumentException("Faktorial tidak didefinisikan untuk bilangan bulat negatif.");
+        }
+        if (n >= 16 ) {
+            throw new IllegalArgumentException("Faktorial tidak didefinisikan untuk > 16.");
+        }
+    int fac = 1;
+    for (int i=n; i>0; i--)
+        fac *= i;
+        return fac;
+    }
+}
